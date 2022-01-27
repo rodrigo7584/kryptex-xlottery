12e65da0a3027c962a678819ee52b838d22ed0d5
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2022-01-27 17:19:59 -0300

    finalizado

--- js/custom.js
+++ js/custom.js
@@ -2,17 +2,6 @@ var etiqueta = document.getElementById('etiqueta-menu'),
   menuLateral = document.getElementById('menu-lateral')
 
 etiqueta.addEventListener('click', function () {
-  menuLateral.classList.toggle('esconder')
-  etiqueta.classList.toggle('esconder')
+  menuLateral.classList.toggle('mostrar')
+  etiqueta.classList.toggle('mostrar')
 })
-
-// onclick="this.classList.toggle('up');
-
-//var btns = paginador.getElementsByClassName('indicador')
-// for (var i = 0; i < btns.length; i++) {
-//   btns[i].addEventListener('click', function () {
-//     var current = document.getElementsByClassName('active')
-//     current[0].className = current[0].className.replace(' active', '')
-//     this.className += ' active'
-//   })
-// }
